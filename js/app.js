43888d99607535330ffa06c2149fa2e0da8bed8a
retroxr74-ui <retroxr74@gmail.com>
2026-08-05 16:22:02 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+window.onload = function () {
 
+    const user = JSON.parse(localStorage.getItem("demoUser"));
+
+    if (!user) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const nameElement = document.getElementById("username");
+
+    if (nameElement) {
+        nameElement.innerHTML = user.name;
+    }
+
+};
